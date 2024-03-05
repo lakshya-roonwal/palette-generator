50604806e56fb9d3b187139ce610ee16cd915633
lakshya-roonwal <erenpool7214@gmail.com>
2024-03-05 13:26:55 +0530

    feat: Add Shadcn UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
     <>
       <h1 className="">Hello World</h1>
+      <div className="p-2">
+      <Button>Hello</Button>
+      </div>
     </>
   )
 }
