8e562e220b7b3feda7fc790a367d801d9a410108
lakshya-roonwal <erenpool7214@gmail.com>
2024-03-13 21:40:55 +0530

    feat : Basic Home Page UI (need design changes)

---
+++ src/components/PopoverPicker.jsx
@@ -0,0 +1,80 @@
+import React, { useCallback, useRef, useState } from "react";
+import { HexAlphaColorPicker } from "react-colorful";
+import { useEffect } from "react";
+
+export const PopoverPicker = ({
+  color,
+  onChange,
+  width = "100%",
+  height = "100%",
+}) => {
+  const popover = useRef();
+  const [isOpen, toggle] = useState(false);
+
+  const close = useCallback(() => toggle(false), []);
+  useClickOutside(popover, close);
+
+  return (
+    <div className="relative">
+      <div
+        className="w-[28px] h-[28px] rounded-sm cursor-pointer "
+        style={{
+          width: width,
+          height: height,
+          backgroundColor: color,
+          border: "3px solid #fff",
+          boxShadow:
+            "0 0 0 1px rgba(0, 0, 0, 0.1), inset 0 0 0 1px rgba(0, 0, 0, 0.1)",
+        }}
+        onClick={() => toggle(true)}
+      />
+
+      {isOpen && (
+        <div
+          style={{
+            top: "calc(100%+2px)",
+            boxShadow: "0 6px 12px rgba(0, 0, 0, 0.15)",
+          }}
+          className="absolute left-0 rounded-sm"
+          ref={popover}
+        >
+          <HexAlphaColorPicker color={color} onChange={onChange} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+// Improved version of https://usehooks.com/useOnClickOutside/
+const useClickOutside = (ref, handler) => {
+  useEffect(() => {
+    let startedInside = false;
+    let startedWhenMounted = false;
+
+    const listener = (event) => {
+      // Do nothing if `mousedown` or `touchstart` started inside ref element
+      if (startedInside || !startedWhenMounted) return;
+      // Do nothing if clicking ref's element or descendent elements
+      if (!ref.current || ref.current.contains(event.target)) return;
+
+      handler(event);
+    };
+
+    const validateEventStart = (event) => {
+      startedWhenMounted = ref.current;
+      startedInside = ref.current && ref.current.contains(event.target);
+    };
+
+    document.addEventListener("mousedown", validateEventStart);
+    document.addEventListener("touchstart", validateEventStart);
+    document.addEventListener("click", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", validateEventStart);
+      document.removeEventListener("touchstart", validateEventStart);
+      document.removeEventListener("click", listener);
+    };
+  }, [ref, handler]);
+};
+
+export default useClickOutside;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,91 +1,32 @@
 import { UserButton, useClerk } from "@clerk/clerk-react";
-import React from "react";
-import {
-  CardTitle,
-  CardDescription,
-  CardHeader,
-  Card,
-} from "@/components/ui/card";
-import DashboardNavbar from "@/components/ui/dashboard/DashboardNavbar";
+import React, { useState } from "react";
+import DashboardNavbar from "@/components/dashboard/DashboardNavbar";
+import { PopoverPicker } from "@/components/PopoverPicker";
+
 const Dashboard = () => {
   const { user } = useClerk();
-
-  {
-    /* <UserButton /> */
-  }
+  const [color, setColor] = useState("#aabbcc");
   return (
     <div className="min-h-screen bg-gray-100">
       <DashboardNavbar />
       <div className="py-6">
-        <header>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <h2 className="text-2xl font-semibold leading-tight text-gray-900">
-              Create Your Palette
-            </h2>
-          </div>
-        </header>
         <main>
           <div className="max-w-4xl mx-auto sm:px-6 lg:px-8">
             <div className="px-4 py-6 sm:px-0">
-              <h3 className="text-xl font-semibold text-gray-900 mb-4">
-                Color Palette
-              </h3>
-              <div className="rounded-lg h-96">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                  <Card className="bg-[#FF0000] text-white">
-                    <CardHeader>
-                      <CardTitle>Primary</CardTitle>
-                      <CardDescription>#FF0000</CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="bg-[#333333] text-white">
-                    <CardHeader>
-                      <CardTitle>Primary Content</CardTitle>
-                      <CardDescription>#333333</CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="bg-[#E0E0E0] text-gray-900">
-                    <CardHeader>
-                      <CardTitle>Primary Light</CardTitle>
-                      <CardDescription>#E0E0E0</CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="bg-[#0000FF] text-white">
-                    <CardHeader>
-                      <CardTitle>Secondary</CardTitle>
-                      <CardDescription>#0000FF</CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="bg-[#3533FF] text-white">
-                    <CardHeader>
-                      <CardTitle>Secondary Light</CardTitle>
-                      <CardDescription>#3533FF</CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="bg-[#2020CC] text-white">
-                    <CardHeader>
-                      <CardTitle>Secondary Dark</CardTitle>
-                      <CardDescription>#2020CC</CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="bg-[#00FF00] text-white">
-                    <CardHeader>
-                      <CardTitle>Success</CardTitle>
-                      <CardDescription>#00FF00</CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="bg-[#FFFF00] text-gray-900">
-                    <CardHeader>
-                      <CardTitle>Warning</CardTitle>
-                      <CardDescription>#FFFF00</CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="bg-[#FF0000] text-white">
-                    <CardHeader>
-                      <CardTitle>Error</CardTitle>
-                      <CardDescription>#FF0000</CardDescription>
-                    </CardHeader>
-                  </Card>
+              <div className="my-4">
+                <h2 className="text-2xl font-bold">Select Your Colors</h2>
+              </div>
+              <div>
+                <div className="flex -justify-between">
+                  <div>
+                    <p className="font-semibold">Color 1</p>
+                    <PopoverPicker
+                      color={color}
+                      onChange={setColor}
+                      width="28px"
+                      height="28px"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
