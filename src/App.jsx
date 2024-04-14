16e478b46764f90fd827a32571c0ee8fde28b6dc
lakshya-roonwal <erenpool7214@gmail.com>
2024-04-14 14:53:59 +0530

    refactor : Formating changes and Removing Routing Comments

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Home />} />
-      {/* <Route
+      <Route
         path="/dashboard/*"
         element={
           <>
@@ -24,7 +24,7 @@ const router = createBrowserRouter(
             </SignedOut>
           </>
         }
-      /> */}
+      />
     </>,
   ),
 );
